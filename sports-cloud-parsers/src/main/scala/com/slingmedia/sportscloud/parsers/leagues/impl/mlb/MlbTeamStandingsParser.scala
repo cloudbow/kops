ddbacfadbd5808526dca35ddac29cc7288d2a4e6
Arun George <arun.george@echostar.com>
2018-01-12 17:00:02 +0530

    Refactor teamstandings parser
    1. Refactor to reduce duplicate code
    2. Reuse as much as possible

--- sports-cloud-parsers/src/main/scala/com/slingmedia/sportscloud/parsers/leagues/impl/mlb/MlbTeamStandingsParser.scala
+++ sports-cloud-parsers/src/main/scala/com/slingmedia/sportscloud/parsers/leagues/impl/mlb/MlbTeamStandingsParser.scala
@@ -1,46 +1,28 @@
 package com.slingmedia.sportscloud.parsers.leagues.impl.mlb
 
 import com.slingmedia.sportscloud.parsers.factory.ParsedItem
-import scala.xml.Elem
-import org.apache.kafka.connect.source.SourceRecord
+import com.slingmedia.sportscloud.parsers.leagues.delegates.ts.impl.mlb.MlbTeamStandingsParserDelegate
 import com.slingmedia.sportscloud.parsers.model.{ League , TeamStandings }
+
+import scala.xml.Elem
 import scala.collection.JavaConverters._
+import scala.xml.NodeSeq
+
+import org.apache.kafka.connect.source.SourceRecord
 import org.apache.kafka.connect.data.Schema
 import org.apache.kafka.connect.data.Struct
 import org.apache.kafka.connect.data.SchemaBuilder
+
 import org.slf4j.LoggerFactory;
 import com.typesafe.scalalogging.slf4j.Logger
-import scala.xml.NodeSeq
 
 class MlbTeamStandingsParser extends ParsedItem {
 
-  private val log = LoggerFactory.getLogger("ScheduleParser")
+  private val log = LoggerFactory.getLogger("MlbTeamStandingsParser")
 
   override def generateRows(data: Elem, in: SourceRecord, xmlRoot: NodeSeq): java.util.List[SourceRecord] = {
-    val leagueStr = (data \\ "league" \ "@alias").text
-
-    var teamStandingsRows = scala.collection.mutable.ListBuffer.empty[SourceRecord]
-    xmlRoot.map { leagueStandings =>
-      val subLeague = (leagueStandings \ "@league").text
-      val mlbDivisionStandingsRows = (leagueStandings \\ "baseball-mlb-division-standings").map {
-        mlbDivisionStandings =>
-          val division = (mlbDivisionStandings \ "@division").text
-          (mlbDivisionStandings \\ "baseball-mlb-team-standings").map {
-            teamStandings =>
-              val teamName = (teamStandings \\ "team-name" \ "@name").text
-              val alias = (teamStandings \\ "team-name" \ "@alias").text
-              val teamCity = (teamStandings \\ "team-city" \ "@city").text
-              val teamCode = (teamStandings \\ "team-code" \ "@global-id").text
-              val wins = toInt((teamStandings \\ "wins" \ "@number").text).getOrElse(0)
-              val losses = toInt((teamStandings \\ "losses" \ "@number").text).getOrElse(0)
-              val pct = toFloat((teamStandings \\ "winning-percentage" \ "@percentage").text).getOrElse(0f)
-              val message = TeamStandings(alias,leagueStr, subLeague, division, teamName, teamCity, teamCode, wins, losses, pct)
-              teamStandingsRows += new SourceRecord(in.sourcePartition, in.sourceOffset, in.topic, 0, in.keySchema, in.key, message.connectSchema, message.getStructure)
-          }
-      }
-    }
-    teamStandingsRows.toList.asJava
-
+    log.trace("Running MlbTeamStandingsParser")
+    new MlbTeamStandingsParserDelegate().generateRows(data,in,xmlRoot)
   }
 
 }
--- sports-cloud-parsers/src/main/scala/com/slingmedia/sportscloud/parsers/leagues/impl/nfl/NflTeamStandingsParser.scala
+++ sports-cloud-parsers/src/main/scala/com/slingmedia/sportscloud/parsers/leagues/impl/nfl/NflTeamStandingsParser.scala
@@ -1,6 +1,7 @@
 package com.slingmedia.sportscloud.parsers.leagues.impl.nfl
 
 import com.slingmedia.sportscloud.parsers.factory.ParsedItem
+import com.slingmedia.sportscloud.parsers.leagues.delegates.ts.impl.nfl.NflTeamStandingsParserDelegate
 import com.slingmedia.sportscloud.parsers.model.{ League , TeamStandings }
 import org.apache.kafka.connect.data.{Schema, SchemaBuilder, Struct}
 import org.apache.kafka.connect.source.SourceRecord
@@ -14,34 +15,8 @@ class NflTeamStandingsParser extends ParsedItem {
   private val log = LoggerFactory.getLogger("ScheduleParser")
 
   override def generateRows(data: Elem, in: SourceRecord, xmlRoot: NodeSeq): java.util.List[SourceRecord] = {
-    val leagueStr = (data \\ "league" \ "@alias").text
-
-    var teamStandingsRows = scala.collection.mutable.ListBuffer.empty[SourceRecord]
-    xmlRoot.map { leagueStandings =>
-      (leagueStandings \\ "football-nfl-conference-standings").map { conferenceStanding =>
-        val subLeague = (conferenceStanding \ "@name").text
-
-        val mlbDivisionStandingsRows = (conferenceStanding \\ "football-nfl-division-standings").map {
-          mlbDivisionStandings =>
-            val division = (mlbDivisionStandings \ "@name").text
-            (mlbDivisionStandings \\ "football-nfl-team-standings").map {
-              teamStandings =>
-                val teamName = (teamStandings \\ "team-name" \ "@name").text
-                val teamCity = (teamStandings \\ "team-city" \ "@city").text
-                val alias = (teamStandings \\ "team-name" \ "@alias").text
-                //val teamCity = (teamStandings \\ "team-city" \ "@city").text
-                val teamCode = (teamStandings \\ "team-code" \ "@global-id").text
-                val wins = toInt((teamStandings \\ "wins" \ "@number").text).getOrElse(0)
-                val losses = toInt((teamStandings \\ "losses" \ "@number").text).getOrElse(0)
-                val pct = toFloat((teamStandings \\ "winning-percentage" \ "@percentage").text).getOrElse(0f)
-                val message = TeamStandings(alias, leagueStr, subLeague, division, teamName, teamCity, teamCode, wins, losses, pct)
-                teamStandingsRows += new SourceRecord(in.sourcePartition, in.sourceOffset, in.topic, 0, in.keySchema, in.key, message.connectSchema, message.getStructure)
-            }
-        }
-      }
-    }
-    teamStandingsRows.toList.asJava
-
+   log.trace("Running NflTeamStandingsParser")
+    new NflTeamStandingsParserDelegate().generateRows(data,in,xmlRoot)
   }
 
 }
